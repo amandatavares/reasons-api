ac0e8e13ade1e117ac5139034c229727ee864f80
amandatvt <amandatvt@gmail.com>
2019-11-12 16:52:58 -0300

    Create categories scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :users
   resources :reasons
 
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
 end
 
 #   Rails.application.routes.draw do
+  resources :categories
 #   resources :users, param: :_username
 
 # end
